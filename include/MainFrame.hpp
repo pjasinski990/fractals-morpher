5aebacd116263a6e7b82fed80eac4a5772156c1d
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-05-15 23:56:42 +0200

    menu load/save bound

--- include/MainFrame.hpp
+++ include/MainFrame.hpp
@@ -8,9 +8,12 @@ public:
     ~MainFrame();
 
 private:
+    enum Id {ID_LOAD, ID_SAVE};
     wxPanel* m_main_panel;
     wxPanel* m_canvas;
     wxMenuBar* m_menu_bar;
     wxMenu* m_file_menu;
 
+    void onLoadMenuClicked(wxCommandEvent& e);
+    void onSaveMenuClicked(wxCommandEvent& e);
 };
--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -8,12 +8,31 @@ MainFrame::MainFrame(wxString title):
         m_menu_bar{new wxMenuBar()},
         m_file_menu{new wxMenu()}
 {
-    m_file_menu->Append(wxID_ANY, wxT("&Load"), wxT("Load fractal( from file"));
-    m_file_menu->Append(wxID_ANY, wxT("&Save"), wxT("Save to file"));
+    m_file_menu->Append(Id::ID_LOAD, wxT("&Load"), wxT("Load fractal from file"));
+    m_file_menu->Append(Id::ID_SAVE, wxT("&Save"), wxT("Save to file"));
 
     m_menu_bar->Append(m_file_menu, wxT("&File"));
     SetMenuBar(m_menu_bar);
-
+    
+    Bind(wxEVT_MENU, &MainFrame::onLoadMenuClicked, this, Id::ID_LOAD);
+    Bind(wxEVT_MENU, &MainFrame::onSaveMenuClicked, this, Id::ID_SAVE);
 }
 
-MainFrame::~MainFrame() = default;
+MainFrame::~MainFrame() 
+{
+    Unbind(wxEVT_COMMAND_BUTTON_CLICKED, &MainFrame::onLoadMenuClicked, this, Id::ID_LOAD);
+    Unbind(wxEVT_COMMAND_BUTTON_CLICKED, &MainFrame::onSaveMenuClicked, this, Id::ID_SAVE);
+}
+
+void MainFrame::onLoadMenuClicked(wxCommandEvent& e)
+{
+    wxFileDialog* dialog = new wxFileDialog(this, wxT("Load File"));
+    dialog->ShowModal();
+
+    e.Skip();
+}
+
+void MainFrame::onSaveMenuClicked(wxCommandEvent& e)
+{
+    e.Skip();
+}
