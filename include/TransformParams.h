7f026b9cbfa94892f65f20e29811c0240ad1c4b2
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-05-16 18:44:51 +0200

    loading files probz working

--- include/TransformParams.h
+++ include/TransformParams.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <vector>
 #include <array>
+#include <memory>
 
 struct Transformation
 {
-    std::vector<std::array<int, 6>> transformation; // m transformation arrays for each fractal 
+    int transform_count;
+    std::vector<std::array<double, 6>> transformation; // m transformation arrays for each fractal 
+    int frames_for_animation; // frames for transition to next 
+    std::shared_ptr<Transformation> next = nullptr;
 };
 
 struct TransformParams
@@ -12,11 +16,7 @@ struct TransformParams
     std::array<int, 2> bitmap_size;
     int iter_count;
     bool is_3d;
-    std::array<int, 3> observer_pos;
+    std::array<double, 3> observer_pos;
     int fractals_count;
-
-    std::vector<int> transformations_for_fractal_count; // n entries for n fractals
-    std::vector<int> frames_for_transition; // n-1 entries for n fractals
-    std::vector<Transformation> transformations_for_fractal; // n for n fractals, each entry has m transformations
-
+    std::vector<std::shared_ptr<Transformation>> transformations_for_fractal; // n for n fractals, each entry has m transformations
 };
--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -31,10 +31,40 @@ void MainFrame::onLoadMenuClicked(wxCommandEvent& e)
             wxEmptyString, wxT("Data files (*.dat)|*.dat"), wxFD_OPEN | wxFD_FILE_MUST_EXIST);
     if (dialog.ShowModal() == wxID_CANCEL) {return;}
 
-    std::fstream fstream(dialog.GetPath());
+    std::ifstream fin(dialog.GetPath());
+    if (!fin.is_open())
+    {
+        // handle, log error 
+        return;
+    }
 
-    fstream >> m_transform.bitmap_size[0] >> m_transform.bitmap_size[1] >> m_transform.iter_count, m_transform.is_3d;
+    fin >> m_transform.bitmap_size[0] >> m_transform.bitmap_size[1] >> m_transform.iter_count >> m_transform.is_3d;
+    fin >> m_transform.observer_pos[0] >> m_transform.observer_pos[1] >> m_transform.observer_pos[2];
+    fin >> m_transform.fractals_count;
 
+    for (int i = 0; i < m_transform.fractals_count; i++)
+    {
+        auto ptr = std::make_shared<Transformation>();
+        fin >> ptr->transform_count;
+        for (int j = 0; j < ptr->transform_count; j++)
+        {
+            std::array<double, 6> arr;
+            fin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] >> arr[5];
+            // std::cout << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << " " << arr[5] << std::endl;
+            ptr->transformation.push_back(arr);
+        }
+        if (i != m_transform.fractals_count-1)  // if not last fractal
+        {
+            fin >> ptr->frames_for_animation;
+            // std::cout << "frames for animation " << i << ": " << ptr->frames_for_animation << std::endl;
+        }
+        if (i > 0)
+        {
+            m_transform.transformations_for_fractal.at(i-1)->next = ptr;
+        }
+        m_transform.transformations_for_fractal.push_back(ptr);
+    }
+    fin.close();
     e.Skip();
 }
 
@@ -44,9 +74,15 @@ void MainFrame::onSaveMenuClicked(wxCommandEvent& e)
             wxEmptyString, wxT("Data files (*.dat)|*.dat"), wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
     if (dialog.ShowModal() == wxID_CANCEL) {return;}
 
-    std::fstream fstream(dialog.GetPath());
+    std::ofstream fout(dialog.GetPath());
+    if (!fout.is_open())
+    {
+        // handle, log error 
+        return;
+    }
 
     // TODO save to file
     
+    fout.close();
     e.Skip();
 }
