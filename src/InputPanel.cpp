bea0ad47cbe644b76e02a853109b4a78cd83231d
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-06-12 19:39:37 +0200

    almost saving from gui

--- src/InputPanel.cpp
+++ src/InputPanel.cpp
@@ -1,6 +1,7 @@
 #include "InputPanel.hpp"
 #include "design.hpp"
 #include "MainFrame.hpp"
+#include "MainPanel.hpp"
 #include "Animation.hpp"
 #include "Fractal.hpp"
 
@@ -89,6 +90,15 @@ InputPanel::InputPanel(wxWindow* parent): wxPanel(parent),
 void InputPanel::onSaveButtonClicked(wxCommandEvent& e)
 {
     MainFrame::animation = getInputFields();
+    MenuPanel* q = dynamic_cast<MenuPanel*>(GetParent());
+    if (q)
+    {
+        MainPanel* mainq = dynamic_cast<MainPanel*>(q->GetParent());
+        if (mainq)
+        {
+            mainq->setInfoText(MainFrame::animation.toString());
+        }
+    }
 }
 
 void InputPanel::setInputFields()
@@ -122,5 +132,40 @@ void InputPanel::setInputFields()
 
 Animation InputPanel::getInputFields() const
 {
-    return Animation();
+    Animation a;
+    a.is_3d = 0;
+    a.fractals_count = 2;
+
+    a.bitmap_size.SetWidth(wxAtoi(m_bitmapsize_x->GetValue()));
+    a.bitmap_size.SetHeight(wxAtoi(m_bitmapsize_y->GetValue()));
+    a.iter_count = wxAtoi(m_points_count->GetValue());
+
+    Fractal f1;
+    f1.transform_count = wxAtoi(m_f1_tcount->GetValue());
+    f1.frames_for_animation = wxAtoi(m_frames_to_second->GetValue());
+    for (int i = 0; i < f1.transform_count; i++)
+    {
+        std::array<double, 6> transform;
+        for (int j = 0; j < 6; j++)
+        {
+            transform[i] = wxAtof(m_transforms1[i][j]->GetValue());
+        }
+        f1.transformations.push_back(transform);
+    }
+    a.fractals.push_back(f1);
+
+    Fractal f2;
+    f2.transform_count = wxAtoi(m_f2_tcount->GetValue());
+    for (int i = 0; i < f2.transform_count; i++)
+    {
+        std::array<double, 6> transform;
+        for (int j = 0; j < 6; j++)
+        {
+            transform[i] = wxAtof(m_transforms2[i][j]->GetValue());
+        }
+        f2.transformations.push_back(transform);
+    }
+    a.fractals.push_back(f2);
+
+    return a;
 }
